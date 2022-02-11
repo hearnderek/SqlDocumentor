a3900a184004bd04a279e57967cee9123a544520
hearnderek <hearnderek@yahoo.com>
2022-02-11 10:17:11 +0900

    Added test project and updated project log

--- ProjectLog.md
+++ ProjectLog.md
@@ -1,5 +1,19 @@
+# Project Summary
+
+
+# Tasks
+
+
+# Event Log
+
 [2022-02-11]
-1. Created project
-2. Decided on project goals
+1. [Action] Created project
+2. [Decision] Decided on project goals
    1. get more familier with Microsoft.SqlServer.Management.SqlParser
    2. build a tool that can document the computation flow through a complex SQL Script
+3. [Decision] Use a Project log 
+   1. Descending INI style date headers 
+   2. Numbered events within day
+   3. ignoring time granularity
+   4. INI style tags at beginning of lines
+4. [Action] Added Test Project
---
+++ SqlDocumentor/TestSqlDocumentor/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestSqlDocumentor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSqlDocumentor")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66e7c28d-d8f7-48e5-b055-33f99be0f6d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
